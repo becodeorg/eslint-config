4dd10f9bb98a9ae1e7bda567f049d73b6f17cdf6
leny <info@flatland.be>
2019-04-22 20:06:31 +0200

    Add --with-hook flag to install husky and lint-staged

--- src/init.js
+++ src/init.js
@@ -1,37 +1,52 @@
 #!/usr/bin/env node
-/* becodeorg/cli
+/* becodeorg/eslint-config
  *
- * /src/index.js - Main entry point
+ * /src/init.js - Init script - Setup ESLint & Prettier
  *
  * coded by leny@BeCode
  * started at 24/10/2018
  */
 
+/* eslint-disable no-console */
+
+import {promisify} from "util";
 import path from "path";
 import fs from "fs";
 import {confirm} from "enquirer";
 import chalk from "chalk";
 import {spawnSync} from "child_process";
 
+const writeFile = promisify(fs.writeFile);
+
 const ESLINTRC = ".eslintrc.js";
+const PACKAGE_JSON = "package.json";
 
 const {peerDependencies} = require("../package.json");
 
 (async () => {
-    const ok = await confirm({
+    const withHook = process.argv.includes("--with-hook");
+
+    const ok = await confirm( {
         name: "ok",
-        message: `This will create an ${chalk.cyan(
-            ".eslintrc.js",
-        )} file here and install the needed dependencies. Are you ok?`,
+        message: [
+            `This will create an ${chalk.cyan(
+                ".eslintrc.js",
+            )} file here and install the needed dependencies.`,
+            withHook
+            ? `It will also setup a ${chalk.yellow("pre-commit")} hook.`
+            : "",
+            "Are you ok?",
+        ].join(" "),
         initial: false,
-    });
+    }
+    );
 
     if (!ok) {
         console.log(chalk.red("Abort."), "Bye.");
         process.exit();
     }
 
-    fs.writeFileSync(
+    await writeFile(
         path.resolve(process.cwd(), ESLINTRC),
         `
 module.exports = {
@@ -41,13 +56,39 @@ module.exports = {
         "utf8",
     );
 
+    const dependencies = Object.keys(peerDependencies);
+
+    withHook && dependencies.push("husky", "lint-staged");
+
     spawnSync(
         "npm",
-        ["i", "--save-dev", "@becode/eslint-config"].concat(
-            Object.keys(peerDependencies),
-        ),
+        ["i", "--save-dev", "@becode/eslint-config"].concat(dependencies),
         {stdio: "inherit"},
     );
+
+    if (withHook) {
+        const packagePath = path.resolve(process.cwd(), PACKAGE_JSON);
+
+        await writeFile(
+            packagePath,
+            JSON.stringify(
+                {
+                    ...require(packagePath), // eslint-disable-line global-require
+                    "lint-staged": {
+                        "*.js": ["npx eslint --fix --cache", "git add"],
+                    },
+                    husky: {
+                        hooks: {
+                            "pre-commit": "npx lint-staged",
+                        },
+                    },
+                },
+                null,
+                2,
+            ),
+            "utf8",
+        );
+    }
 
     console.log("🎉", chalk.green("Success!"));
 })();
